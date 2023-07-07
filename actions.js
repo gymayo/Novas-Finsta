ff81878d5a03c45aa8719d89499afc1ded592382
gymayo <101837853+gymayo@users.noreply.github.com>
2023-07-06 22:33:49 -0400

    Add files via upload

---
+++ actions.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Get a reference to the button
+    var aboutButton = document.getElementById("aboutButton");
+  
+    // Add a click event listener to the button
+    aboutButton.addEventListener("click", function() {
+      // Redirect to "aboutNova.html"
+      window.location.href = "aboutNova.html";
+    });
+  });
+  
